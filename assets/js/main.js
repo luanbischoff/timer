6a75caadc2d05a7e5941990a5c4a0183c02d916a
luanbischoff <luanbschff@gmail.com>
2022-06-27 10:54:45 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const clockElement = document.querySelector('.relogio');
+
+let seconds = 0;
+let timer;
+
+function createSeconds(seconds) {
+    const data = new Date(seconds * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC',
+    });
+}
+function startClock() {
+    timer = setInterval(function () {
+        seconds++;
+        clockElement.innerText = createSeconds(seconds);
+    }, 1000);
+    clockElement.classList.remove('pausado');
+}
+function pauseClock() {
+    clearInterval(timer);
+    clockElement.classList.add('pausado');
+}
+function resetClock() {
+    clearInterval(timer);
+    seconds = 0;
+    clockElement.classList.remove('pausado');
+    clockElement.innerText = '00:00:00';
+}
